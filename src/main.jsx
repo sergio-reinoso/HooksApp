f2ef228259eca55122f2eaa25a5d9876517b5e08
Sergio Reinoso <slrs993@gmail.com>
2023-11-02 22:44:37 -0500

    Optimizacion del codigo useRef y useLayoutEffect mejor practica usar useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 
 
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MultipleCustomHooks />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <Layout />
+  // </React.StrictMode>,
 )
