55dba1a3d00b1534086f8c173585c3b2de8af19a
Sergio Reinoso <slrs993@gmail.com>
2023-11-16 23:28:53 -0500

    Creacion de nuevos componentes, instacion de react router, configuracion de react router

---
+++ src/useContext/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const LoginPage = () => {
+  return (
+    <>
+        <h1>Login Page</h1>
+        <hr />
+    </>
+  )
+}
---
+++ src/useContext/MainApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { HomePage } from './HomePage'
+import { AboutPage } from './AboutPage'
+import { LoginPage } from './LoginPage'
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>Main App</h1>
+        <hr />
+
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='about' element={<AboutPage />} />
+            <Route path='login' element={<LoginPage />} />
+
+            {/* <Route path='/*' element={ <LoginPage /> } /> */}
+            <Route path='/*' element={ <Navigate to='/about' /> } />
+        </Routes>
+    </>
+  )
+}
