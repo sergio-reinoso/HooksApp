b62a42e5bc97023d793a820fd71c97639e4ab944
Sergio Reinoso <slrs993@gmail.com>
2023-11-21 22:58:35 -0500

    Pruebas en el hook useCounter

---
+++ test/hooks/useCounter.test.js
@@ -0,0 +1,26 @@
+import { renderHook } from "@testing-library/react"
+import { useCounter } from "../../src/hooks/useCounter"
+
+describe('Pruebas en el hook useCounter', () => {
+
+    test('Debe de retonar los valores por defecto', () => {
+
+        const { result } = renderHook( () => useCounter() )
+        const { counter, decrement, increment, reset } = result.current
+
+        expect( counter ).toBe(10)
+        expect( decrement ).toEqual(expect.any( Function ))
+        expect( increment ).toEqual(expect.any( Function ))
+        expect( reset ).toEqual(expect.any( Function ))
+
+    })
+
+    test('Debe de generar el counter con el valor de 100', () => {
+
+        const { result } = renderHook( () => useCounter(100) )
+        const { counter} = result.current
+
+        expect( counter ).toBe(100)
+    })
+
+})
